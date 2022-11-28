fd3a4e876940f817ba27fc1b9dd805aab881883a
yakdd <yakdd@yandex.ru>
2022-11-28 19:16:21 +0300

    method CreateSecondArray added

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -21,6 +21,23 @@
 }
 
 
+string[] CreateSecondArray(string[] iniArray, int count, int filter)
+{
+    // Функция формирования Требуемого Массива
+    // Возвращает массив строк длиной менее 'filter' символов
+    string[] array = new string[count];
+    int j = 0;
+    for (int i = 0; i < iniArray.Length; i++)
+    {
+        if (iniArray[i].Length <= filter)
+        {
+            array[j] = iniArray[i];
+            j++;
+        }
+    }
+    return array;
+}
+
 
 void PrintArray(string[] array)
 {
@@ -40,3 +57,13 @@ Console.Write("Сколько строк будет в массиве?: ");
 int countElements = Convert.ToInt32(Console.ReadLine());
 string[] initialArray = CreateStringArray(countElements);
 PrintArray(initialArray);
+
+int maxLength = 3;
+int count = default;
+for (int i = 0; i < initialArray.Length; i++)
+{
+    if (initialArray[i].Length <= maxLength) count++;
+}
+
+string[] secondArray = CreateSecondArray(initialArray, count, maxLength);
+PrintArray(secondArray);
